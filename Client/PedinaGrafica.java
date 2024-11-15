ca4b7d79f585b19817bb0c2e8a01e96948b23d04
MattiaGuggi <mattiahag@gmail.com>
2024-11-15 19:38:50 +0100

    Listener spostato in Campo

--- Client/PedinaGrafica.java
+++ Client/PedinaGrafica.java
@@ -2,8 +2,6 @@ package Client;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import Server.Posizione;
 import Server.Pedina;
