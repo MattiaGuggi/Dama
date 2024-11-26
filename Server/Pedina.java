f7a0ea9dc5ce680e6719af76b8f666a8595023e2
MattiaGuggi <mattiahag@gmail.com>
2024-11-26 21:48:25 +0100

    Aggiornato le mosse

--- Server/Pedina.java
+++ Server/Pedina.java
@@ -41,54 +41,70 @@ public class Pedina {
     public ArrayList<Mossa> getPossibleMoves(Pedina[][] board) {
         int x = posizione.getX();
         int y = posizione.getY();
-        ArrayList<Mossa> allPossibleMoves = new ArrayList<>();
+        ArrayList<Mossa> normalMoves = new ArrayList<>();
+        ArrayList<Mossa> captureMoves = new ArrayList<>();
     
         // null indicate cella vuota
         // Per mangiare deve essere di colore diverso
         if (this.color.equals("white") || this.isDama) {
+            // Destra avanti
             if (y > 0 && x < MAX-1 && board[y-1][x+1] == null) {
-                allPossibleMoves.add(new Mossa(new Posizione(x+1, y-1), false));
+                normalMoves.add(new Mossa(new Posizione(x+1, y-1), false));
             }
             else if (y > 1 && x < MAX-2 && board[y-1][x+1] != null 
-                       && !board[y-1][x+1].getColor().equals(this.color) 
-                       && board[y-2][x+2] == null) {
-                allPossibleMoves.add(new Mossa(new Posizione(x+2, y-2), true));
+                    && !board[y-1][x+1].getColor().equals(this.color) 
+                    && board[y-2][x+2] == null) {
+                captureMoves.add(new Mossa(new Posizione(x+2, y-2), true));
             }
+
+            // Sinistra avanti
             if (y > 0 && x > 0 && board[y-1][x-1] == null) {
-                allPossibleMoves.add(new Mossa(new Posizione(x-1, y-1), false));
+                normalMoves.add(new Mossa(new Posizione(x-1, y-1), false));
             }
             else if (y > 1 && x > 1 && board[y-1][x-1] != null 
-                       && !board[y-1][x-1].getColor().equals(this.color) 
-                       && board[y-2][x-2] == null) {
-                allPossibleMoves.add(new Mossa(new Posizione(x-2, y-2), true));
+                    && !board[y-1][x-1].getColor().equals(this.color) 
+                    && board[y-2][x-2] == null) {
+                captureMoves.add(new Mossa(new Posizione(x-2, y-2), true));
             }
         } 
         if (this.color.equals("black") || this.isDama) {
+            // Destra indietro
             if (y < MAX-1 && x < MAX-1 && board[y+1][x+1] == null) {
-                allPossibleMoves.add(new Mossa(new Posizione(x+1, y+1), false));
+                normalMoves.add(new Mossa(new Posizione(x+1, y+1), false));
             }
             else if (y < MAX-2 && x < MAX-2 && board[y+1][x+1] != null 
-                       && !board[y+1][x+1].getColor().equals(this.color) 
-                       && board[y+2][x+2] == null) {
-                allPossibleMoves.add(new Mossa(new Posizione(x+2, y+2), true));
+                    && !board[y+1][x+1].getColor().equals(this.color) 
+                    && board[y+2][x+2] == null) {
+                captureMoves.add(new Mossa(new Posizione(x+2, y+2), true));
             }
+
+            // Sinistra indietro
             if (y < MAX-1 && x > 0 && board[y+1][x-1] == null) {
-                allPossibleMoves.add(new Mossa(new Posizione(x-1, y+1), false));
+                normalMoves.add(new Mossa(new Posizione(x-1, y+1), false));
             }
             else if (y < MAX-2 && x > 1 && board[y+1][x-1] != null 
-                       && !board[y+1][x-1].getColor().equals(this.color) 
-                       && board[y+2][x-2] == null) {
-                allPossibleMoves.add(new Mossa(new Posizione(x-2, y+2), true));
+                    && !board[y+1][x-1].getColor().equals(this.color) 
+                    && board[y+2][x-2] == null) {
+                captureMoves.add(new Mossa(new Posizione(x-2, y+2), true));
             }
         }
-    
+        
         // Rimuovi mosse al di fuori della board
-        allPossibleMoves.removeIf(move -> {
+        normalMoves.removeIf(move -> {
             Posizione p = move.getTargetPosition();
             return p.getX() < 0 || p.getX() >= MAX || p.getY() < 0 || p.getY() >= MAX;
         });
+        captureMoves.removeIf(move -> {
+            Posizione p = move.getTargetPosition();
+            return p.getX() < 0 || p.getX() >= MAX || p.getY() < 0 || p.getY() >= MAX;
+        });
+
+        // Priorita alle mangiate
+        if (!captureMoves.isEmpty()) {
+            return captureMoves;
+        }
     
-        return allPossibleMoves;
+        return normalMoves;
     }    
 
     @Override
